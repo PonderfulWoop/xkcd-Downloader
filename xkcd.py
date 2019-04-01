aa64b94bde00b9986b9b0f7ad7f9f36b5f45ea7a
Shashank Shirol <shashank.shirol1@gmail.com>
2019-04-01 22:16:25 +0530

    01-04-19

--- xkcd.py
+++ xkcd.py
@@ -6,17 +6,16 @@ current_url = 'http://xkcd.com/info.0.json'
 resp = requests.get(current_url)
 current_num = int(resp.json()['num'])
 
-while(True):
+ip = input("Enter number of comics to download : ")
+k = 0
+while(k < int(ip)):
     rd = random.randint(1, current_num)
     url = 'http://xkcd.com/'+str(rd)+'/info.0.json'
     resp = requests.get(url)
-
+    print('Downloading comic', k)
     urllib.request.urlretrieve(resp.json()['img'], resp.json()['title']+'.png')
-    print('done!!')
-    k = input('press 1 to download another one, 0 to quit: ')
-
-    if int(k) is 0:
-        break
+    print('Done')
+    k = k + 1
 
 print('Thanks for trying out xkcd-Downloader')
 
